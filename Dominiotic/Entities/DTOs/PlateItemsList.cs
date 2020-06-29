fa55f0dc13d0c85f558b1d0319a43839cd4f95f2
Alexander Paulino <alexanderpauul@hotmail.com>
2020-06-29 18:01:40 -0400

    Listas.

---
+++ Dominiotic/Entities/DTOs/PlateItemsList.cs
@@ -0,0 +1,10 @@
+﻿using Dominiotic.Entities.Models;
+using System.Collections.Generic;
+
+namespace Dominiotic.Entities.DTOs
+{
+    public class PlateItemsList
+    {
+        public List<PlatesItems> PlatesItems = new List<PlatesItems>();
+    }
+}
