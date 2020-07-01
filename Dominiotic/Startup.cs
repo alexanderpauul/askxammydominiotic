893303ad3e555f05cd6761bfdd52ff479e2bbb49
Alexander Paulino <alexanderpauul@hotmail.com>
2020-07-01 12:00:44 -0400

    File

--- Dominiotic/Startup.cs
+++ Dominiotic/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dominiotic
 {
@@ -16,6 +19,16 @@ namespace Dominiotic
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Entities.Models.DominioticContext>(options =>
+            options.UseSqlServer("Server=tcp:alpauul.database.windows.net,1433;Initial Catalog=FoodONE;Persist Security Info=False;User ID=alpauul;Password=Alexkils01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.AllowAnyOrigin());
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +37,7 @@ namespace Dominiotic
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage();
             }
 
             app.UseRouting();
@@ -35,6 +49,9 @@ namespace Dominiotic
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            app.UseDefaultFiles();
+            //app.UseMvc();
         }
     }
 }
