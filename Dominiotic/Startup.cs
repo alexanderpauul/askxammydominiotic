aaf1aed2f0ad9461e9b73a310cdddc3f902cce39
Alexander Paulino <alexanderpauul@hotmail.com>
2020-07-02 23:46:06 -0400

    Update postman documentation

--- Dominiotic/Startup.cs
+++ Dominiotic/Startup.cs
@@ -15,12 +15,21 @@ namespace Dominiotic
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<Entities.Models.DominioticContext>(options =>
-            options.UseSqlServer("Server=tcp:alpauul.database.windows.net,1433;Initial Catalog=FoodONE;Persist Security Info=False;User ID=alpauul;Password=Alexkils01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+            options.UseSqlServer(Configuration.GetConnectionString("ContextConnection")));
 
             services.AddCors(options =>
             {
@@ -29,29 +38,43 @@ namespace Dominiotic
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+
+            //services.AddDbContext<Entities.Models.DominioticContext>(options =>
+            //options.UseSqlServer("Server=tcp:alpauul.database.windows.net,1433;Initial Catalog=FoodONE;Persist Security Info=False;User ID=alpauul;Password=Alexkils01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("AllowOrigin",
+            //        builder => builder.AllowAnyOrigin());
+            //});
+
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseDatabaseErrorPage();
             }
 
-            app.UseRouting();
+            //endpoints.MapGet("/", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
 
+            app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "api",
+                    pattern: "{controller}/{id?}");
             });
 
-            app.UseDefaultFiles();
-            //app.UseMvc();
         }
     }
 }
